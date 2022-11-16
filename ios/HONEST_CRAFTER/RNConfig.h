824f9a425154eedd9bf0c67a8449817761d59997
manishreddot <manish@reddotinnovative.com>
2022-11-16 13:16:24 +0530

    Half iOS Env Done TIll native modules

---
+++ ios/HONEST_CRAFTER/RNConfig.h
@@ -0,0 +1,16 @@
+//
+//  RNConfig.h
+//  HONEST_CRAFTER
+//
+//  Created by Developer on 16/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNConfig : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/HONEST_CRAFTER/RNConfig.m
@@ -0,0 +1,12 @@
+//
+//  RNConfig.m
+//  HONEST_CRAFTER
+//
+//  Created by Developer on 16/11/22.
+//
+
+#import "RNConfig.h"
+
+@implementation RNConfig
+
+@end
