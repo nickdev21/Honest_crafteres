830c9763da55b8dc394338c95788a1161894c34f
manishreddot <manish@reddotinnovative.com>
2022-11-16 13:17:24 +0530

    iOS half ENV Done till native modules

--- ios/HONEST_CRAFTER/RNConfig.h
+++ ios/HONEST_CRAFTER/RNConfig.h
@@ -1,16 +1,4 @@
-//
-//  RNConfig.h
-//  HONEST_CRAFTER
-//
-//  Created by Developer on 16/11/22.
-//
-
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface RNConfig : NSObject
-
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+@interface RNConfig : NSObject <RCTBridgeModule>
 @end
-
-NS_ASSUME_NONNULL_END
--- ios/HONEST_CRAFTER/RNConfig.m
+++ ios/HONEST_CRAFTER/RNConfig.m
@@ -8,5 +8,22 @@
 #import "RNConfig.h"
 
 @implementation RNConfig
+RCT_EXPORT_MODULE();
 
+- (NSDictionary *)constantsToExport
+{
+#if DEV
+  NSString *env = @"dev";
+#elif STAGING
+  NSString *env = @"staging";
+#else
+  NSString *env = @"prod";
+#endif
+  
+ return @{ @"env": env };
+}
+
++(BOOL)requiresMainQueueSetup{
+  return YES;
+}
 @end
